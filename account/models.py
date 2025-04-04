a5f85bcd75999bc4da401fea257ef5b184b48744
Masato <113155801+IshikawaMasato@users.noreply.github.com>
2025-04-04 12:15:29 +0900

    初期モデルの作成

--- account/models.py
+++ account/models.py
@@ -1,3 +1,9 @@
+# account/models.py
 from django.db import models
 
-# Create your models here.
+class DatabaseCheck(models.Model):
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
