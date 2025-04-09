fe98bd8fcdda8e6fcab42de95edcef9a236d3d96
佐々木新太 <a.sasaki.sys23@morijyobi.ac.jp>
2025-04-09 11:26:16 +0900

    base.html作成

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from account.views import HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home' , HomeView.as_view(), name="home")
 ]
