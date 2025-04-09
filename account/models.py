a9f7f343fcb260ad5ceb776012b7e4a4717c659c
knagaki <k.nagaki.sys23@morijyobi.ac.jp>
2025-04-09 09:57:47 +0900

    modelsの変更

--- account/models.py
+++ account/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    department_id = models.ForeignKey("review.Department", on_delete=models.CASCADE)
+    department_id = models.ForeignKey("review.Department", on_delete=models.CASCADE, default=1)
     grade = models.IntegerField(default=1)
     icon_url = models.TextField(default='default.jpg')
     language = models.CharField(max_length=20, null=True, blank=True)
