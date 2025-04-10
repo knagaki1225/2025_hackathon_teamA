6a5498188b2647e9ebfbf87fcaa3fa1a51c1f585
Masato <113155801+IshikawaMasato@users.noreply.github.com>
2025-04-10 10:39:32 +0900

    setting.pyに画像保存用のパスを記述

--- config/settings.py
+++ config/settings.py
@@ -143,3 +143,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' 
 AUTH_USER_MODEL = 'account.User'
 
 LOGIN_REDIRECT_URL = '/review/'
+
+#画像保存用のディレクトリ
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
