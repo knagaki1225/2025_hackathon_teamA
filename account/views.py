f9147c545fde7dafce1cab3449699a51599d3c36
knagaki <k.nagaki.sys23@morijyobi.ac.jp>
2025-04-10 09:35:27 +0900

    ログイン、サインアップ機能の実装

--- account/views.py
+++ account/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+# accounts/views.py
+from django.urls import reverse_lazy
+from django.views import generic
+from .forms import CustomUserCreationForm
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+User = get_user_model()
+
+class SignUpView(generic.CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'accounts/signup.html'
