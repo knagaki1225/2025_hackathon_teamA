f056abd616104cdaef76eaf852e010d3f42a1999
Masato <113155801+IshikawaMasato@users.noreply.github.com>
2025-04-10 12:32:17 +0900

    画像編集用のviews.pyの追加

--- account/views.py
+++ account/views.py
@@ -1,7 +1,10 @@
 # accounts/views.py
 from django.urls import reverse_lazy
 from django.views import generic
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .forms import CustomUserCreationForm
+from .forms import ImageUploadForm
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
@@ -10,3 +13,17 @@ class SignUpView(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'accounts/signup.html'
+    
+@login_required  # ログインユーザーのみがアクセスできるようにする
+def upload_icon(request):
+    if request.method == 'POST':
+        form = ImageUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            user = request.user
+            user.icon_url = form.cleaned_data['icon_url']
+            user.save()  # ユーザーのアイコンを保存
+            return redirect('profile')  # アップロード後にプロフィールページにリダイレクト
+    else:
+        form = ImageUploadForm()
+
+    return render(request, 'account/upload_icon.html', {'form': form})
