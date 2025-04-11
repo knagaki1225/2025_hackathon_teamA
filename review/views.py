ac2bf6344e5d14052e86cccfbef472261a817431
knagaki <k.nagaki.sys23@morijyobi.ac.jp>
2025-04-11 10:08:14 +0900

    トップ画面へのデータの送信

--- review/views.py
+++ review/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.views import generic    # 汎用ビューのインポート
 from .models import Review
+from django.db.models import Count, Q
 
 def search():
     render
 
-class IndexView(generic.ListView):
-    model = Review     
-    template_name = 'home.html'    # 使用するテンプレート名を指定
+def review_list(request):
+    reviews = Review.objects.annotate(
+        good_count=Count('good', filter=Q(good__del_flg=False))
+    ).order_by('-good_count')
+
+    return render(request, 'home.html', {'reviews': reviews})
 
 class ReviewDetailView(generic.DetailView):
     model = Review
