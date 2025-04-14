350363a4da6e614dc12785edeb4142e49047efe8
Masato <113155801+IshikawaMasato@users.noreply.github.com>
2025-04-14 10:36:23 +0900

    カスタムビューを使用

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+from .views import (
+    CustomPasswordResetView, CustomPasswordResetDoneView,
+    CustomPasswordResetConfirmView, CustomPasswordResetCompleteView,
+)
 
 app_name = 'account'
 
@@ -8,4 +13,8 @@ urlpatterns = [
     path('upload-icon/', views.upload_icon, name='upload_icon'),  # 画像アップロードページ
     path('all/', views.AllView.as_view(), name='all'),
     path('admin/<int:pk>', views.AccountsDetailView.as_view(), name='accountsDetail'),
+    path('password_reset/', CustomPasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', CustomPasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', CustomPasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
